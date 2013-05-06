1a42e90b5961b4b1d31dfe1332e60bedfcead47b
Nicolas McIntyre <nickmac@Nicolass-MacBook-Pro-2.local>
2013-05-06 00:32:49 -0700

    Version 0.1

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('modules/map.html', {
+  chrome.app.window.create('modules/home.html', {
     id: 'embedder',
     bounds: {
       width: 600,
--- js/controllers.js
+++ js/controllers.js
@@ -12,3 +12,32 @@ function gotoMap(){
   });
 });
 }
+
+var navList = angular.module('navList', []);
+
+navList.controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };        
+}]);
+
+angular.module("navbarapp", ["controllers"])
+  .directive("bootstrapNavbar", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    transclude: true,
+    templateUrl: "components/navbar.html",
+    compile: function(element, attrs) {  // (1)
+      var li, liElements, links, index, length;
+ 
+      liElements = $(element).find("#navigation-tabs li");   // (2)
+      for (index = 0, length = liElements.length; index < length; index++) {
+        li = liElements[index];
+        links = $(li).find("a");  // (3)
+        if (links[0].textContent === attrs.currentTab) $(li).addClass("active"); // (4)
+      }
+    }
+  }});
+;
