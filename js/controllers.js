01c53fbe6c10c20f045fb04bcd419801ab39273b
Nicolas McIntyre <nick@Nicolass-MacBook-Pro.local>
2013-06-23 17:14:20 -0700

    alpha
    navigation now uses angularjs

---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+function NavCtrl($scope) {
+	$scope.modules = ['Location','Community','System','Docs','Help'];
+}
+
+function SysCtrl($scope) {
+	$scope.generators = ['Hydro','Solar','Wind','Biogas'];
+}
