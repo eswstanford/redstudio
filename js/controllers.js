2ce1059a03fca90c7063b7a3e74910e14846db66
Nicolas McIntyre <nick@Nicolass-MacBook-Pro.local>
2013-06-24 17:34:35 -0700

    alpha
    added font awesome and angularjs

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,25 @@ function NavCtrl($scope) {
 	$scope.modules = ['Location','Community','System','Docs','Help'];
 }
 
+function HmCtrl($scope) {
+	$scope.hmscrn = ['Load','New','Information','Settings'];
+}
+
 function SysCtrl($scope) {
 	$scope.generators = ['Hydro','Solar','Wind','Biogas'];
+}
+
+function FormCtrl($scope) { 
+	$scope.master= {};
+	$scope.update = function(user) { 
+		$scope.master= angular.copy(user);
+	};
+	$scope.reset = function() {
+		$scope.user = angular.copy($scope.master);
+	};
+	$scope.reset();
+}
+
+function HomeCtrl($scope) {
+	$scope.projects = ['Project 1','Project 2','Project 3','Project 3','Project 4'];
 }
